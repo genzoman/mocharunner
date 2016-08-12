d52bc60976cb14dfd17cebc8a2a8922dca50bfa9
darrin <darrinthomascecil@gmail.com>
2016-08-12 15:48:51 -0400

    in memory database with a simple database get

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,38 @@
+// import * as express from "express";
+// let app = express();
+// app.get("/",(req, res)=>{
+//   res.send("hello world");
+// });
+// app.listen(9999,()=>{
+//   console.log("listening on port 9999");
+// });
 import * as express from "express";
+
+import * as ReactDOM from "react-dom";
 let app = express();
-app.get("/",(req, res)=>{
-  res.send("hello world");
+import * as sqlite from "sqlite3";
+let db = new sqlite.Database(':memory:');
+import * as fs from "fs";
+
+
+app.get("/", (req, res) => {
+  db.serialize(() => {
+    db.run("create table myTable(info TEXT)");
+    var stmt = db.prepare("INSERT INTO myTable VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+    db.all("select * from myTable", (err, data) => {
+      let json = JSON.stringify(data,null,2);
+      fs.writeFile("./results.txt",json);
+      res.send(json);
+    })
+  })
+
 });
-app.listen(9999,()=>{
-  console.log("listening on port 9999");
+
+
+app.listen(9999, () => {
+  console.log("listening...");
 });
